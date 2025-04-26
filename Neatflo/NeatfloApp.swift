1b7fea033bdf7567bf867a12e3ba5e1bbd4b1f18
Daval <davalcato@yahoo.com>
2025-04-26 15:09:20 -0700

    Initial Commit

---
+++ Neatflo/NeatfloApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeatfloApp.swift
+//  Neatflo
+//
+//  Created by Ethan Hunt on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NeatfloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeatfloTests/NeatfloTests.swift
@@ -0,0 +1,16 @@
+//
+//  NeatfloTests.swift
+//  NeatfloTests
+//
+//  Created by Ethan Hunt on 4/26/25.
+//
+
+import Testing
+
+struct NeatfloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
